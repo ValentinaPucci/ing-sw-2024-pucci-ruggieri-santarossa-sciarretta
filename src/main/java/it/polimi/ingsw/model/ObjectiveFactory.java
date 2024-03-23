a69374d81de926cb6467401d2d21db7bf4156465
margheritasantarossa <margherita.santarossa@mail.polimi.it>
2024-03-23 17:46:34 +0100

    fixed factory

--- src/main/java/it/polimi/ingsw/model/ObjectiveFactory.java
+++ src/main/java/it/polimi/ingsw/model/ObjectiveFactory.java
@@ -2,20 +2,16 @@ package it.polimi.ingsw.model;
 
 public class ObjectiveFactory {
 
-    /*
-    todo: fix the constructor methods in ItemObjectiveCard, LetterPatternObjectiveCard, DiagonalPatternObjectiveCard,
-     ResourceObjectiveCard classes
-     */
-            public ObjectiveCard createObjectiveCard(String type) {
+            public ObjectiveCard createObjectiveCard(String type, int id, Orientation orientation,int points) {
                 switch (type) {
                     case "Item":
-                        return new ItemObjectiveCard();
+                        return new ItemObjectiveCard(id, orientation);
                     case "LetterPattern":
-                        return new LetterPatternObjectiveCard();
+                        return new LetterPatternObjectiveCard(id, orientation, points);
                     case "DiagonalPattern":
-                        return new DiagonalPatternObjectiveCard();
+                        return new DiagonalPatternObjectiveCard(id, orientation, points);
                     case "Resource":
-                        return new ResourceObjectiveCard();
+                        return new ResourceObjectiveCard(id, orientation, points);
                     default:
                         throw new IllegalArgumentException("Invalid objective card type: " + type);
                 }
