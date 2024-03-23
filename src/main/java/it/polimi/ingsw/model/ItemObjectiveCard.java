17ddc88128d2b954c69faf7003ac11eec5c3dda7
riccardo__ruggieri <riccardo.ruggieri@mail.polimi.it>
2024-03-23 18:12:01 +0100

    fix

--- src/main/java/it/polimi/ingsw/model/ItemObjectiveCard.java
+++ src/main/java/it/polimi/ingsw/model/ItemObjectiveCard.java
@@ -6,7 +6,12 @@ public class ItemObjectiveCard extends ObjectiveCard {
      * @param id
      * @param orientation
      */
-    public ItemObjectiveCard(int id, Orientation orientation) {
-        super(id, orientation);
+    public ItemObjectiveCard(int id, Orientation orientation, int points) {
+        super(id, orientation, points);
+    }
+
+    @Override
+    public int calculateScore(PersonalBoard personal_board) {
+        return personal_board.getNum_mushrooms() + personal_board.getNum_leaves();
     }
 }
--- src/main/java/it/polimi/ingsw/model/ResourceObjectiveCard.java
+++ src/main/java/it/polimi/ingsw/model/ResourceObjectiveCard.java
@@ -6,7 +6,13 @@ public class ResourceObjectiveCard extends ObjectiveCard {
      * @param id
      * @param orientation
      */
-    public ResourceObjectiveCard(int id, Orientation orientation) {
-        super(id, orientation);
+    public ResourceObjectiveCard(int id, Orientation orientation, int points) {
+        super(id, orientation, points);
     }
+
+    @Override
+    public int calculateScore(PersonalBoard personal_board) {
+        return personal_board.getNum_mushrooms() + personal_board.getNum_leaves();
+    }
+
 }
