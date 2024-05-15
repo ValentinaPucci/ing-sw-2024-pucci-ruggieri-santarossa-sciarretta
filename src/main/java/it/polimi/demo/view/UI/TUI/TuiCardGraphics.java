5bca802753b0cd70f8f5bee6b66ce69c133a8dc2
lollo-n3ur0 <lorenzo.sciarretta@mail.polimi.it>
2024-05-15 16:05:26 +0200

    card printing finished

--- src/main/java/it/polimi/demo/view/UI/TUI/TuiCardGraphics.java
+++ src/main/java/it/polimi/demo/view/UI/TUI/TuiCardGraphics.java
@@ -5,14 +5,13 @@ public class TuiCardGraphics {
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
-    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m"; // Colore di sfondo personalizzabile
-    public static final String ANSI_RED_BACKGROUND = "\u001B[31m";
-    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[35m";
-    public static final String ANSI_CYAN_BACKGROUND = "\u001B[36m";
-    public static final String ANSI_COLOR_CORNER = "\u001B[41m"; // Colore degli angoli personalizzabile
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 
     // Rappresentazione grafica della carta
-    private static final String[] GRAFICA_CARTA = {
+    private static final String[] CARD_GRAPHICS = {
             "╭──────────────╮",
             "│ %s          %s │",
             "│              │",
@@ -25,7 +24,6 @@ public class TuiCardGraphics {
     private String corner3;
     private String corner4;
 
-
     public TuiCardGraphics(String corner1, String corner2, String corner3, String corner4) {
         this.corner1 = corner1;
         this.corner2 = corner2;
@@ -33,15 +31,24 @@ public class TuiCardGraphics {
         this.corner4 = corner4;
     }
 
-    public void stampaCarta(String Color) {
-        System.out.println(ANSI_RESET + ANSI_BLACK + Color + GRAFICA_CARTA[0] + ANSI_RESET);
-        for (int i = 1; i < GRAFICA_CARTA.length - 1; i++) {
-            if (i < 3)
-                System.out.println(ANSI_RESET + ANSI_BLACK + Color + String.format(GRAFICA_CARTA[i], corner1, corner2) + ANSI_RESET);
-            else
-                System.out.println(ANSI_RESET + ANSI_BLACK + Color + String.format(GRAFICA_CARTA[i], corner3, corner4) + ANSI_RESET);
+
+    public void printCard(String Color) {
+        System.out.println(ANSI_RESET + CARD_GRAPHICS[0] + ANSI_RESET);
+        for (int i = 1; i < CARD_GRAPHICS.length - 1; i++) {
+            if (i == 1){
+                String row = String.format(CARD_GRAPHICS[i], corner1, corner2);
+                System.out.println(ANSI_RESET  + row.substring(0,4) + Color+ row.substring(4,12)+ Color+ ANSI_RESET + row.substring(12,16) + ANSI_RESET);
+            }
+            else if(i== 3){
+                String row3 = String.format(CARD_GRAPHICS[i], corner3, corner4);
+                //String colored_row3 = coloraRiga(String.format(CARD_GRAPHICS[i], corner3, corner4), Color, 2, 3);
+                System.out.println(ANSI_RESET  + row3.substring(0,4) + Color+ row3.substring(4,12)+ Color+ ANSI_RESET + row3.substring(12,16) + ANSI_RESET);
+            } else{
+                String row4 = String.format(CARD_GRAPHICS[i]);
+                System.out.println(ANSI_RESET  + row4.substring(0,2) + Color+ row4.substring(2,14)+ Color+ ANSI_RESET + row4.substring(14,16) + ANSI_RESET);
+            }
         }
-        System.out.println(ANSI_RESET + ANSI_BLACK + Color + GRAFICA_CARTA[4] + ANSI_RESET);
+        System.out.println(ANSI_RESET + CARD_GRAPHICS[4] + ANSI_RESET);
         System.out.println("\n");
     }
 
@@ -50,6 +57,6 @@ public class TuiCardGraphics {
          String ANSI_COLOR_BACKGROUND = "\u001B[42m";
 
         TuiCardGraphics carta = new TuiCardGraphics("R", "B", "R", "T");
-        carta.stampaCarta(ANSI_WHITE_BACKGROUND); // Stampa la carta con sfondo colorato
+        carta.printCard(ANSI_CYAN_BACKGROUND); // Stampa la carta con sfondo colorato
     }
 }
