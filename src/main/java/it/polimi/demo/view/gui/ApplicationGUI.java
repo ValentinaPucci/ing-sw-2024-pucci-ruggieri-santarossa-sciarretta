3db2162cbc8e6fe7bfbe43ba233d9a6ffa79df4d
valepu2 <113843853+valepu2@users.noreply.github.com>
2024-05-28 15:11:49 +0200

    SceneInfo created

--- src/main/java/it/polimi/demo/view/gui/ApplicationGUI.java
+++ src/main/java/it/polimi/demo/view/gui/ApplicationGUI.java
@@ -1,4 +1,21 @@
 package it.polimi.demo.view.gui;
 
-public class ApplicationGUI {
+import it.polimi.demo.view.flow.GameFlow;
+import javafx.application.Application;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+
+public class ApplicationGUI extends Application {
+
+    private GameFlow gameFlow;
+
+    private Stage primaryStage, popUpStage;
+    private StackPane root;
+    private ArrayList<SceneInfo> scenes;
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
 }
